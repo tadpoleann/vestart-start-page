8e307b6983c5ba784da039f365ba7e30c72a97d9
tadpoleann <laesybab@gmail.com>
2022-03-23 16:09:37 -0700

    laksjdflakjs

--- js/script.js
+++ js/script.js
@@ -8,31 +8,31 @@ let btn3 = document.getElementById("btn3"),
   frame3 = document.getElementById("frame3");
 
 btn1.addEventListener("click", () => {
-  img.src = "./pics/paintinga.png";
+  img.src = "./pics/paintingasmaller-min.jpg";
 });
 
 btn2.addEventListener("click", () => {
   console.log("changed to image b");
-  img.src = "./pics/paintingb.png";
+  img.src = "./pics/paintingbsmaller-min.jpg";
 });
 
 btn3.addEventListener("click", () => {
-  img.src = "./pics/paintingc.png";
+  img.src = "./pics/paintingcsmaller-min.jpg";
 });
 
 frame1.addEventListener("click", () => {
-  img.style.borderImage = "url(./pics/frame1.png) 100 round";
+  img.style.borderImage = "url(./pics/frame1-min.png) 100 round";
 });
 
 frame2.addEventListener("click", () => {
-  img.style.borderImage = "url(./pics/frame2.png) 100 round";
+  img.style.borderImage = "url(./pics/frame2-min.png) 100 round";
 });
 
 frame3.addEventListener("click", () => {
-  img.style.borderImage = "url(./pics/frame3.png) 100 round";
+  img.style.borderImage = "url(./pics/frame3-min.png) 100 round";
 });
 
-// change pics in carousel
+// Change pics in carousel
 const carouselImg = document.querySelector(".current-img-container img");
 
 const changePicFw = () => {
@@ -40,12 +40,12 @@ const changePicFw = () => {
   carouselImg.onanimationend = () => {
     carouselImg.classList.remove("opacity");
   };
-  if (carouselImg.src.match("./carousel/Slide1.jpg")) {
-    carouselImg.src = "./carousel/Slide2.jpg";
-  } else if (carouselImg.src.match("./carousel/Slide2.jpg")) {
-    carouselImg.src = "./carousel/Slide3.jpg";
+  if (carouselImg.src.match("./carousel/Slide1smallersize-min.jpg")) {
+    carouselImg.src = "./carousel/Slide2-min.jpg";
+  } else if (carouselImg.src.match("./carousel/Slide2-min.jpg")) {
+    carouselImg.src = "./carousel/Slide3smallersize-min.jpg";
   } else {
-    carouselImg.src = "./carousel/Slide1.jpg";
+    carouselImg.src = "./carousel/Slide1smallersize-min.jpg";
   }
 };
 
@@ -54,11 +54,11 @@ const changePicBw = () => {
   carouselImg.onanimationend = () => {
     carouselImg.classList.remove("opacity");
   };
-  if (carouselImg.src.match("./carousel/Slide3.jpg")) {
-    carouselImg.src = "./carousel/Slide2.jpg";
-  } else if (carouselImg.src.match("./carousel/Slide2.jpg")) {
-    carouselImg.src = "./carousel/Slide1.jpg";
+  if (carouselImg.src.match("./carousel/Slide3smallersize-min.jpg")) {
+    carouselImg.src = "./carousel/Slide2-min.jpg";
+  } else if (carouselImg.src.match("./carousel/Slide2-min.jpg")) {
+    carouselImg.src = "./carousel/Slide1smallersize-min.jpg";
   } else {
-    carouselImg.src = "./carousel/Slide3.jpg";
+    carouselImg.src = "./carousel/Slide3smallersize-min.jpg";
   }
 };
